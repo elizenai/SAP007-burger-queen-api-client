c015d22267b6b62038ad7441e79e84363fe3b62c
Elizenai Silva <elizenai.silva@hotmail.com>
2022-05-15 20:37:06 -0300

    feat: WIP add style page login
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: GabrielaMedrado <gabrieladmr@gmail.com>

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import "./style.css";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { Layout } from "../../components/Layout";
 import { userLogin } from "../../services/api";
+import { Label } from "../../components/Label";
+import image from "../../img/logo-removebg-preview.png";
 
 export const Login = () => {
     const [email, setEmail] = useState("");
@@ -21,9 +24,21 @@ export const Login = () => {
       
     return (
     <Layout>
-      <Input placeholder= "Digite seu email" type= "email"value= {email} onChange={(e) => setEmail(e.target.value)}/>
-      <Input placeholder= "*******" type= "password" value= {password} onChange={(e) => setPassword(e.target.value)}/>
-      <Button className="button" btnText="Entrar" onClick = {handleClick}/>
+      <section className="all-content">
+        <figure>
+          <img src= {image} alt="logo"/>
+        </figure>
+        <h1 className="text-form"> Acesse sua conta </h1>
+        <div className="container">
+          <form className="form-container">
+            <Label className="textLabel">Email</Label>
+            <Input placeholder= "Digite seu email" type= "email"value= {email} onChange={(e) => setEmail(e.target.value)}/>
+            <Label className="label-password">Senha:</Label>
+            <Input placeholder= "*******" type= "password" value= {password} minLength="6" onChange={(e) => setPassword(e.target.value)}/>
+            <Button className="button" btnText="Entrar" onClick = {handleClick}/>
+          </form>
+        </div>
+      </section>
     </Layout>
   ); 
 };
