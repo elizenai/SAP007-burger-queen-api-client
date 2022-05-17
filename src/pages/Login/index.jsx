1103cebba592c3e06189c03d3245f9a197d9b8de
Elizenai Silva <elizenai.silva@hotmail.com>
2022-05-16 22:45:38 -0300

    feat: routes adjustments
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: GabrielaMedrado <gabrieladmr@gmail.com>

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -5,40 +5,67 @@ import { Input } from "../../components/Input";
 import { Layout } from "../../components/Layout";
 import { userLogin } from "../../services/api";
 import { Label } from "../../components/Label";
-import image from "../../img/logo-removebg-preview.png";
+import image from "../../img/logo-hamburger.png";
+import { statusError } from "../../services/error";
+import { setToken } from "../../services/token";
+import { ErrorMessage } from "../../components/ErrorMessage";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [codeErro, setErro] = useState("");
+  const navigate = useNavigate();
 
-    const handleClick = (e) => {
-      e.preventDefault();
-      userLogin(email, password)
+  const handleClick = (e) => {
+    e.preventDefault();
+    userLogin(email, password)
       .then((response) => {
-      console.log(response);
-      window.open("/register");     
+        if (response.status === 200) {
+          return response.json();
+        }
+        setErro(statusError(response));
+      })
+      .then((data) => {
+        setToken(data.token);
+        navigate(data.role === "saloon" ? "/saloon" : "/kitchen");
       })
       .catch((error) => console.log(error));
     // mostrar o erro
-    };
-      
-    return (
+  };
+
+  return (
     <Layout>
       <section className="all-content">
         <figure>
-          <img src= {image} alt="logo"/>
+          <img src={image} alt="logo" />
         </figure>
         <h1 className="text-form"> Acesse sua conta </h1>
         <div className="container">
           <form className="form-container">
             <Label className="textLabel">Email</Label>
-            <Input placeholder= "Digite seu email" type= "email"value= {email} onChange={(e) => setEmail(e.target.value)}/>
+            <Input
+              placeholder="Digite seu email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <Label className="label-password">Senha:</Label>
-            <Input placeholder= "*******" type= "password" value= {password} minLength="6" onChange={(e) => setPassword(e.target.value)}/>
-            <Button className="button" btnText="Entrar" onClick = {handleClick}/>
+            <Input
+              placeholder="*******"
+              type="password"
+              value={password}
+              minLength="6"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button className="button" btnText="Entrar" onClick={handleClick} />
           </form>
+          <ErrorMessage
+          disable={codeErro ? false : true}
+          message={codeErro}
+          />
         </div>
       </section>
     </Layout>
-  ); 
+  );
 };
