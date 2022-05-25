9b2a1d73d747062d87b0294f6f54ffd20593d12d
Gabriela Medrado <gabrieladmr@gmail.com>
2022-05-25 18:04:22 -0300

    testes iniciais

--- src/components/Button/Button.test.jsx
+++ src/components/Button/Button.test.jsx
@@ -2,7 +2,6 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Button from "./";
 
-// eslint-disable-next-line no-undef
 describe("Button Component", () => {
   
   it("Render a button correctly", () => {
