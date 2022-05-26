35f826ede18732e2a335b8651ac32b1dc1ad48c8
Elizenai Silva <elizenai.silva@hotmail.com>
2022-05-26 16:42:55 -0300

    feat: add onClick ProductCard
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: GabrielaMedrado <gabrieladmr@gmail.com>

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,16 +1,15 @@
 import { Button } from "../Button";
 import "./style.css";
-export const ProductCard = ({image, price, name}) => {
+export const ProductCard = ({image, price, name, onClick}) => {
   return (
     <li className="product">
-      <img className="product-img" src={image} alt={name}/>
-      <p className="product-name">{name}</p>
-      <p className="product-price">R$ {price},00 </p>
-       {/* <div>{children}</div> */}
+       <img className="product-img" src={image} alt={name}/>
+       <p className="product-name">{name}</p>
+       <p className="product-price">R$ {price},00 </p>
        <Button
        type="submit"
        className="btn-add-product"
-      //  onClick={}
+       onClick={onClick}
        >Adicionar</Button>
     </li>
   );
