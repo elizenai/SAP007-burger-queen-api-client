8c22235aaf0623adeb1d11ad498bf5e21377245d
Elizenai Silva <elizenai.silva@hotmail.com>
2022-05-30 22:30:48 -0300

    docs: indentation correction
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: GabrielaMedrado <gabrieladmr@gmail.com>

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -3,14 +3,14 @@ import "./style.css";
 export const ProductCard = ({image, price, name, onClick}) => {
   return (
     <li className="product">
-       <img className="product-img" src={image} alt={name}/>
-       <p className="product-name">{name}</p>
-       <p className="product-price">R$ {price},00 </p>
-       <Button
+      <img className="product-img" src={image} alt={name}/>
+      <p className="product-name">{name}</p>
+      <p className="product-price">R$ {price},00 </p>
+      <Button
        type="submit"
        className="btn-add-product"
        onClick={onClick}
-       >Adicionar</Button>
+      >Adicionar</Button>
     </li>
   );
 };
