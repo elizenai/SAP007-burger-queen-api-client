883bac8edafec70f692077f0ba9f5bb4eb88e3f0
Elizenai Silva <elizenai.silva@hotmail.com>
2022-06-07 22:37:23 -0300

    feat: create component order kitchen
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: GabrielaMedrado <gabrieladmr@gmail.com>

---
+++ src/components/OrdersKitchen/index.jsx
@@ -0,0 +1,12 @@
+// export const OrdersKitchen = () => {
+//   return (
+//     <li>
+//       <p>Cliente:</p>
+//       <p>Mesa</p>
+//       <p>Criação do pedido:</p>
+//       <p>Status dos pedido:</p>
+//       <p>Ultima atualização</p>
+
+//     </li>
+//   );
+// };
