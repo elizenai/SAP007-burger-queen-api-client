7d02168999c9c316089b921adcfe4221e8052478
Elizenai Silva <elizenai.silva@hotmail.com>
2022-06-08 22:43:08 -0300

    feat: adjustment component OrderKitchen
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: GabrielaMedrado <gabrieladmr@gmail.com>

--- src/components/OrdersKitchen/index.jsx
+++ src/components/OrdersKitchen/index.jsx
@@ -1,26 +1,25 @@
-import { Command } from "../Command";
+import "./style.css";
 
 export const OrdersKitchen = ({client, table, products, createdAt, updateAt, status}) => {
   return (
-    <li>
+  
+    <li className="order-list">
       <p>Cliente: {client}</p>
       <p>Mesa: {table}</p>
-      <p>Produtos: {products}</p>
       <p>Criação do pedido: {createdAt}</p>
       <p>Status dos pedido: {status}</p>
       <p>Última atualização: {updateAt}</p>
-      {products.map((product) => {
+      <ul>Produtos: {products.map((product) => {
         return (
           <div key={product.id}>
-            <Command
-            name={product.client}
-            flavor={product.flavor}
-            complement={product.complement}
-            qtd={product.qtd}
-            />
+            <p>{product.name}</p>
+            <p>{product.flavor}</p>
+            <p>{product.complement}</p>
+            <p>{product.qtd}</p>
           </div>
         );
-      })};
-    </li>
+        })}
+      </ul>
+    </li> 
   );
 };
