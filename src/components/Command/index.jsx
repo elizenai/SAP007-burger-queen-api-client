764819d22ed74a917688d1f25771088606bd7c22
Elizenai Silva <elizenai.silva@hotmail.com>
2022-06-13 11:32:41 -0300

    refactor: components Command and OrderKitchen
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: GabrielaMedrado <gabrieladmr@gmail.com>

--- src/components/Command/index.jsx
+++ src/components/Command/index.jsx
@@ -2,15 +2,14 @@ export const Command = ({ id, name, price, flavor, complement, qtd }) => {
   return (
     <ul>
       <li> 
+        <p>{qtd} <span>{name}</span> </p>
         <p>{id}</p>
-        <p>{name}</p>
         <p>{flavor}</p>
         <p>{complement}</p>
       </li> 
         
       <li>
         <p>R$ {price},00</p>
-        <p>{qtd}</p>
       </li>     
     </ul>
   );
