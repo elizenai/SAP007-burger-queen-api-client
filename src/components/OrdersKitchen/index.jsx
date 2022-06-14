2d87e3df9a8089d4ff20d13c9b1f00cac4c89101
Elizenai Silva <elizenai.silva@hotmail.com>
2022-06-14 20:27:19 -0300

    feat: add button finish
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: GabrielaMedrado <gabrieladmr@gmail.com>

--- src/components/OrdersKitchen/index.jsx
+++ src/components/OrdersKitchen/index.jsx
@@ -1,29 +1,33 @@
+import { Button } from "../Button";
 import "./style.css";
 
-export const OrdersKitchen = ({client, table, products, createdAt, updatedAt, status}) => {
+export const OrdersKitchen = ({client, table, products, createdAt, updatedAt, status, onClick}) => {
   return (
   
     <li className="order-list">
+      <div className="container-order-kitchen">
+        <div className="container-details-order">
+          <p className="details-order"><span className="details-order-span">Cliente:</span> {client}</p>
+          <p className="details-order"><span className="details-order-span">Mesa:</span> {table}</p>
+          <p className="details-order"> <span className="details-order-span">Criação do pedido:</span> {createdAt}</p>
+          <p className="details-order"><span className="details-order-span">Status do pedido:</span> {status}</p>
+          <p className="details-order"><span className="details-order-span">Última atualização:</span> {updatedAt}</p>
+        </div>
+      
+        <ul className="style-products">Produtos: {products.map((product) => {
 
-      <div className="container-details-order">
-        <p className="details-order"><span className="details-order-span">Cliente:</span> {client}</p>
-        <p className="details-order"><span className="details-order-span">Mesa:</span> {table}</p>
-        <p className="details-order"> <span className="details-order-span">Criação do pedido:</span> {createdAt}</p>
-        <p className="details-order"><span className="details-order-span">Status do pedido:</span> {status}</p>
-        <p className="details-order"><span className="details-order-span">Última atualização:</span> {updatedAt}</p>
+          return (
+            <div key={product.id}>
+              <p>{product.qtd} <span>{product.name}</span></p>
+              <p>{product.flavor}</p>
+              <p>{product.complement}</p>
+              
+            </div>
+          );
+          })}
+        </ul>
       </div>
-      <ul className="style-products">Produtos: {products.map((product) => {
-
-        return (
-          <div key={product.id}>
-            <p>{product.qtd} <span>{product.name}</span></p>
-            <p>{product.flavor}</p>
-            <p>{product.complement}</p>
-            
-          </div>
-        );
-        })}
-      </ul>
+      <Button className="btn-finish" value="finish" onClick={onClick}> Finalizar</Button>
     </li> 
   );
 };
