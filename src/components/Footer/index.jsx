7812f12d835031642371f05b6b4ffe1a6f02f2b0
Gabriela Medrado <gabrieladmr@gmail.com>
2022-06-20 17:34:59 -0300

    Style component footer
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: GabrielaMedrado <gabrieladmr@gmail.com>

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,8 +3,8 @@ import "./style.css";
 export const Footer = () => {
   return(
     <footer className="footer">
-      <p>Desenvolvido por <a href="https://github.com/elizenai">Elizenai Silva</a>
-        e<a href="https://github.com/GabrielaMedrado">Gabriela Medrado</a>
+      <p>Desenvolvido por <a href="https://github.com/elizenai">Elizenai Silva </a>
+        e <a href="https://github.com/GabrielaMedrado"> Gabriela Medrado</a>
         </p>
     </footer>
   );
